b29f21d2b3f66d9b8925d5fc4b809fcd279b51c5
Chris Imgarten <cjimgarten@gmail.com>
2017-07-22 15:09:15 -0500

    move main method to Main class

---
+++ src/main/java/com/cjimgarten/blackjack/Main.java
@@ -0,0 +1,13 @@
+package com.cjimgarten.blackjack;
+
+/**
+ * Created by chris on 7/18/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Blackjack game = new Blackjack();
+        game.start();
+    }
+
+}
